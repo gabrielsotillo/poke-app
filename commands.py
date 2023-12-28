9cc1e7bfb8721b77fac701c8e1f54fd2733f4634
Gabriel Sotillo <gsotillo.developer@gmail.com>
2023-12-28 00:33:25 -0400

    gui improvements and new button (clear)

--- commands.py
+++ commands.py
@@ -1,10 +1,12 @@
+import requests
+import customtkinter
+from io import BytesIO
+from PIL import Image
+
 def get_pokemon():
 
-    import requests
-    import customtkinter
-    from io import BytesIO
-    from PIL import Image
-    from gui import my_entry, my_label
+
+    from gui import my_entry, my_label, my_textbox
 
     # Dictionary about the Pokemon requested
     r = requests.get(f'https://pokeapi.co/api/v2/pokemon/{my_entry.get().lower()}')
@@ -15,11 +17,26 @@ def get_pokemon():
     r = requests.get(img_url)
     pokemon_img = r.content 
 
-    # Image Widget update 
-    my_image = customtkinter.CTkImage(light_image=Image.open(BytesIO(pokemon_img)), size=(150, 150))
+    # TextBox Widget Update
+    name = pokemon['name'].capitalize()
+    weight = pokemon['weight']
+    height = pokemon['height']
+    type = pokemon['types'][0]['type']['name'].capitalize()
+
+    my_textbox.delete("0.0","end")
+    my_textbox.insert("0.0", f"Name: {name}\nType: {type}\nWeight: {weight}\nHeight: {height}")
+
+    # Image Widget Update 
+    my_image = customtkinter.CTkImage(light_image=Image.open(BytesIO(pokemon_img)), size=(310, 310))
     my_label.configure(image=my_image)
 
     print(pokemon['name'])
+
+def clear():
+    from gui import my_label, my_textbox
+
+    my_textbox.delete("0.0","end")
+    my_label.configure(image=None)
 
 def exit_app():
     exit()
--- gui.py
+++ gui.py
@@ -8,50 +8,48 @@ def run_GUI():
     customtkinter.set_appearance_mode('dark')
     customtkinter.set_default_color_theme('blue')
     root = customtkinter.CTk()
-    root.geometry('500x400')
+    root.geometry('800x600')
     root.title('PokeDex')
 
     # ---------------------- Widgets ----------------------
-    frame = customtkinter.CTkFrame(master=root)
-    frame.pack(pady=20, padx=60, fill='both', expand=True)
+    left_frame = customtkinter.CTkFrame(master=root, width=400, fg_color="#dd2626")
+    left_frame.pack(side='left', fill='both')
+
+    right_frame = customtkinter.CTkFrame(master=root, width=400, height=600, fg_color='#d03939')
+    right_frame.pack(fill='both', expand=True)
+    
+    top_frame = customtkinter.CTkFrame(master=right_frame, width=400, height=300, border_width=20, border_color='#d03939', fg_color="#ffffff")
+    top_frame.pack(fill='both', expand=True)
+
+    bottom_frame = customtkinter.CTkFrame(master=right_frame, width=400, height=300, fg_color=  '#d03939'  )
+    bottom_frame.pack(fill='both', expand=True)
 
     # Label
     global my_label 
-    my_label = customtkinter.CTkLabel(master=frame, text='', height=150)
-    my_label.pack(pady=10)
+    my_label = customtkinter.CTkLabel(master=top_frame, text='')
+    my_label.place(relx = 0.5, rely = 0.5, anchor = 'center')
 
     # Entry
     global my_entry 
-    my_entry = customtkinter.CTkEntry(master=frame, placeholder_text='Pokemon')
-    my_entry.pack(pady=12, padx=80)
+    my_entry = customtkinter.CTkEntry(master=bottom_frame, placeholder_text='Pokemon')
+    my_entry.pack(pady=20)
 
     # Buttons
-    global search_button, exit_button 
-    search_button = customtkinter.CTkButton(master=frame, text='Search', command=get_pokemon)
-    search_button.pack(pady=12, padx=10)
-    exit_button = customtkinter.CTkButton(master=frame, text='Exit', command=exit_app)
-    exit_button.pack(pady=12, padx=10)
+    global search_button, clear_buttom, exit_button 
+    search_button = customtkinter.CTkButton(master=bottom_frame, text='Search', command=get_pokemon)
+    search_button.pack(pady=6)
+    clear_button = customtkinter.CTkButton(master=bottom_frame, text='Clear', command=clear)
+    clear_button.pack(pady=6)
+    exit_button = customtkinter.CTkButton(master=bottom_frame, text='Exit', command=exit_app)
+    exit_button.pack(pady=6, side='bottom')
+
+    # TextBox
+    global my_textbox
+    my_textbox = customtkinter.CTkTextbox(master=left_frame, width=350, height=550, font=('Helvetica', 25), text_color='black', fg_color='white')
+    my_textbox.place(relx = 0.5, rely = 0.5, anchor = 'center')
+
     # ------------------------------------------------------
 
     root.mainloop()
 
-
-
-'''
-    label = customtkinter.CTkLabel(master=frame, text='Login System', font=('Roboto', 25))
-    label.pack(pady=12, padx=18)
-
-    entry_1 = customtkinter.CTkEntry(master=frame, placeholder_text='Username')
-    entry_1.pack(pady=12, padx=80)
-
-    entry_2 = customtkinter.CTkEntry(master=frame, placeholder_text='Password', show='*')
-    entry_2.pack(pady=12, padx=10)
-
-    button = customtkinter.CTkButton(master=frame, text='Login', command=login)
-    button.pack(pady=12, padx=10)
-
-    checkbox = customtkinter.CTkCheckBox(master=frame, text='Remember me')
-    checkbox.pack(pady=12, padx=10)
-
-'''
 
