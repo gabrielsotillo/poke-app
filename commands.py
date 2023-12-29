b1dc9b3ea2c10cb9dd1a9054fba5fcc5250c4501
Gabriel Sotillo <gsotillo.developer@gmail.com>
2023-12-28 22:25:38 -0400

    improved get_info method

--- commands.py
+++ commands.py
@@ -1,13 +1,15 @@
 import requests
+#import inspect
 
 class Pokemon:
 
     def __init__(self, pokemon):
 
         self.name = pokemon['name'].capitalize()
-        self.weight = pokemon['weight']
-        self.height = pokemon['height']
+        self.weight = f"{pokemon['weight']} lb"
+        self.height = f"{pokemon['height']} in"
         self.type = pokemon['types'][0]['type']['name'].capitalize()
+        self.id = pokemon['id']
         self.image = self.get_img(pokemon) 
 
     def get_img(self, pokemon):
@@ -17,12 +19,15 @@ class Pokemon:
         return r.content 
     
     def get_info(self):
-         info = (f"Name: {self.name}\n"
-                 f"Type: {self.type}\n"
-                 f"Weight: {self.weight} lb\n"
-                 f"Height: {self.height} in")
 
-         return info
+        info = ""
+        for key in vars(self):
+            if key == 'image':
+                continue
+            else:
+                info = info + f"{key.capitalize()}: {vars(self)[key]}\n"
+         
+        return info
         
 
 def get_pokemon(pokename):
